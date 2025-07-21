ccdeeecf7a94c3ed557ecf110cb0d3e16c37729a
Jonathyn Major <jlmajor@cdsvisual.com>
2025-07-20 17:03:44 -0700

    add deployment file

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
-  },
+  base: '/',
   server: {
     port: 3000,
   },
